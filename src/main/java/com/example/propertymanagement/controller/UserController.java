f624c50755afdcd2605b1f17beeef2eb317c90ef
Amir, Mohammed <r621549@regence.com>
2023-02-16 21:25:17 -0800

    logs and actuators

--- src/main/java/com/example/propertymanagement/controller/UserController.java
+++ src/main/java/com/example/propertymanagement/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.propertymanagement.controller;
 
 import com.example.propertymanagement.dto.UserDTO;
 import com.example.propertymanagement.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,12 +21,15 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @ApiOperation(value = "register", notes = "This method is used for registration of user")
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> register(@Valid @RequestBody UserDTO userDTO){
+    public ResponseEntity<UserDTO> register(
+            @ApiParam(name = "user", type = "userDTO", example = "user information", required = true
+            ) @Valid @RequestBody UserDTO userDTO){
         return new ResponseEntity<>(userService.register(userDTO), HttpStatus.CREATED);
     }
 
-    @PostMapping("/login")
+    @PostMapping(path = "/login", consumes = {"application/json"}, produces = {"application/json"})
     public ResponseEntity<UserDTO> login(@RequestBody UserDTO userDTO){
         return new ResponseEntity<>(userService.login(userDTO.getOwnerEmail(), userDTO.getPassword()), HttpStatus.OK);
     }
